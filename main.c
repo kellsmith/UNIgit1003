915a1bc5c4d444a4ebd47206020b4a322f9d0c1c
kellsmith <kelljoseph2000@hotmail.com>
2020-02-25 18:59:29 +1100

    initial comment

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float x;
+    float y;
+    printf("enter a number: ");
+
+
+
+
+
+
+
+
+
+
+
+    scanf("%f", &x);
+    y = ((9*x)+3)*5;
+    printf("y: %f\n", y);
+    return 0;
+}
+// this is hard!!
